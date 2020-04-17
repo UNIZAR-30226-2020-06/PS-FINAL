5d41060bfad734b2aaac823ce5c708bee74b4364
Kifixo <danielhuici@hotmail.com>
2020-04-18 00:42:31 +0200

    Dani: sincronizacion de servlets 5

---
+++ src/com/espotify/dao/JSONAdapter.java
@@ -0,0 +1,30 @@
+package com.espotify.dao;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.json.JSONObject;
+
+public class JSONAdapter {
+	public static JSONObject parsarJSON(HttpServletRequest request) {
+		JSONObject peticionJSON = null;
+		
+	     try {
+	    	 StringBuilder sb = new StringBuilder();
+		     String s;
+		     
+			while ((s = request.getReader().readLine()) != null) {
+				 sb.append(s);
+			 }
+			
+			 String parseJSON = sb.toString();
+			 peticionJSON = new JSONObject(parseJSON);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    
+	    return peticionJSON;
+	}
+}
